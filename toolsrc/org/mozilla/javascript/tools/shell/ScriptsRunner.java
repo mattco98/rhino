965e7cad2f93f76493724bc73c6fb137fe75f013
Matthew Olsson <matthewcolsson@gmail.com>
2023-05-15 09:47:54 -0700

    Add a simple ScriptsRunner class

---
+++ toolsrc/org/mozilla/javascript/tools/shell/ScriptsRunner.java
@@ -0,0 +1,46 @@
+package org.mozilla.javascript.tools.shell;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.commonjs.module.Require;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScriptsRunner extends Main {
+    public static void main(String[] _args) {
+        File scriptIndex = new File("./scripts/index.js");
+
+        ContextFactory cxFactory = new ScriptsRunnerContextFactory();
+        Context cx = cxFactory.enterContext();
+
+        Global global = new Global();
+        global.init(cx);
+
+        List<String> modulePath = new ArrayList<>();
+        modulePath.add(scriptIndex.toURI().getPath());
+        Require require = global.installRequire(cx, modulePath, false);
+
+        require.getExportedModuleInterface(cx, "index", scriptIndex.toURI(), null, false);
+        Context.exit();
+    }
+
+    private static class ScriptsRunnerContextFactory extends ContextFactory {
+        @Override
+        protected void onContextCreated(Context cx) {
+            super.onContextCreated(cx);
+
+            cx.setLanguageVersion(Context.VERSION_ES6);
+        }
+
+        @Override
+        protected boolean hasFeature(Context cx, int featureIndex) {
+            if (featureIndex == Context.FEATURE_LOCATION_INFORMATION_IN_ERROR) {
+                return true;
+            }
+
+            return super.hasFeature(cx, featureIndex);
+        }
+    }
+}
